e7061e4ebd7c88a514c4f2420a83b4c1baa9429d
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-03-19 10:59:52 +0100

    nav slider aside better

--- ion_radionica/main.js
+++ ion_radionica/main.js
@@ -1,16 +1,22 @@
 $('.owl-carousel').owlCarousel({
     loop:true,
     margin:50,
-    nav:true,
+    nav:false,
+    dots:false,
+    autoplay: true,
+    autoplayTimeout: 2500,
+    autoplaySpeed: 1000,
+    animateIn: "fadeIn",
+    animateOut: "fadeOut",
     responsive:{
         0:{
             items:1
         },
         600:{
-            items:2
+            items:1
         },
         1000:{
-            items:2
+            items:1
         }
     }
 })
